392926e9a73880bddf19017c3da49c86d804e6a4
egoing <egoing@gmail.com>
2023-11-20 16:17:31 +0900

    상세보기 구현

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -3,6 +3,13 @@ type ReadProps = {
     id: string;
   };
 };
-export default function Read(props: ReadProps) {
-  return <div>Read {props.params.id}</div>;
+export default async function Read(props: ReadProps) {
+  const res = await fetch("http://localhost:9999/posts/" + props.params.id);
+  const post = await res.json();
+  return (
+    <div>
+      <h2>{post.title}</h2>
+      {post.body}
+    </div>
+  );
 }
