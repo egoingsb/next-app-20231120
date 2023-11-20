129b9ec704d7b3714861b18b6912b7d391ea235a
egoing <egoing@gmail.com>
2023-11-20 17:25:12 +0900

    삭제 기능 구현

--- src/app/Control.tsx
+++ src/app/Control.tsx
@@ -1,9 +1,10 @@
 "use client";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export default function Control() {
   const params = useParams();
+  const router = useRouter();
   console.log(params);
   return (
     <ul>
@@ -16,7 +17,18 @@ export default function Control() {
             <Link href={"/update/" + params.id}>update</Link>
           </li>
           <li>
-            <button>delete</button>
+            <button
+              onClick={() => {
+                fetch("http://localhost:9999/posts/" + params.id, {
+                  method: "DELETE",
+                }).then(() => {
+                  router.push("/");
+                  router.refresh();
+                });
+              }}
+            >
+              delete
+            </button>
           </li>
         </>
       ) : null}
