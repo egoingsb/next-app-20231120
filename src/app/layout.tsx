ba9cf321a675a5a1614a9c3a88e4b7eef3c6e91e
egoing <egoing@gmail.com>
2023-11-20 13:18:04 +0900

    Link 컴포넌트를 이용해서 페이지 깜박거림을 막을 수 있고, 부분적으로 필요한 컨텐트만 다운로드 할 수 있다. 이미 다운 받은 페이지는 캐슁을 통해서 네트워크를 절감할 수 있고, 성능을 높일 수 있다.
    https://nextjs.org/docs/app/building-your-application/routing/linking-and-navigating

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,23 +16,24 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <h1>
-          <a href="/">WEB</a>
+          <Link href="/">WEB</Link>
         </h1>
+        <input type="text" placeholder="search" />
         <ul>
           <li>
-            <a href="/read/1">html</a>
+            <Link href="/read/1">html</Link>
           </li>
           <li>
-            <a href="/read/2">css</a>
+            <Link href="/read/2">css</Link>
           </li>
         </ul>
         <article>{children}</article>
         <ul>
           <li>
-            <a href="/create">create</a>
+            <Link href="/create">create</Link>
           </li>
           <li>
-            <a href="/update/1">update</a>
+            <Link href="/update/1">update</Link>
           </li>
           <li>
             <button>delete</button>
